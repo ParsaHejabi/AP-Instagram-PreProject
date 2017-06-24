d57703b9139e01d4bf1d037ec3249022b66af56e
parsahejabi <parsa.hejabi@gmail.com>
2017-06-24 17:58:23 +0430

    make homePageController

--- src/Client/homePageController.java
+++ src/Client/homePageController.java
@@ -1,14 +1,31 @@
 package Client;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * Created by parsahejabi on 6/23/17.
  */
-public class homePageController {
+public class homePageController implements Initializable {
+    @FXML
+    ListView<VBox> posts;
+
+    static ArrayList<Post> postArrayList;
+
     public void goToProfile1() throws IOException, ClassNotFoundException {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("profilePage1.fxml")));
         scene.getStylesheets().add("Stylesheet/style.css");
@@ -35,5 +52,52 @@ public class homePageController {
         Client.clientOutputStream.writeUTF("Search");
         Client.clientOutputStream.flush();
         Client.refreshOwner();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (Post p:postArrayList){
+            VBox post = new VBox(20);
+            HBox postOwnerHBox = new HBox(30);
+            Circle postOwnerProfilePicture = new Circle(35,new ImagePattern(new Image(p.owner.profilePicture.toURI().toString())));
+            Hyperlink postOwnerUsername = new Hyperlink(p.owner.username);
+            postOwnerHBox.getChildren().addAll(postOwnerProfilePicture,postOwnerUsername);
+            ImageView postImageView = new ImageView(new Image(p.image.toURI().toString()));
+            postImageView.setFitWidth(500);
+            postImageView.setFitHeight(500);
+            HBox postButtonsHBox = new HBox(10);
+            ImageView likeButtonImageView = new ImageView("Assets/likeButton.png");
+            if (p.canComment){
+                ImageView commentButtonImageView = new ImageView("Assets/commentButton.png");
+                postButtonsHBox.getChildren().addAll(likeButtonImageView, commentButtonImageView);
+            }
+            else {
+                postButtonsHBox.getChildren().add(likeButtonImageView);
+            }
+            Hyperlink likesHyperlink = new Hyperlink(p.liked.size() + " likes");
+            Hyperlink postOwnerUsernameHyperLink = new Hyperlink(p.owner.username);
+            TextArea postCaptionTextArea = new TextArea(p.caption);
+            //TODO age khastim ye kari konim ke date alano begire menhaye oon moghe kone
+            Label postDateLabel = new Label(p.uploadDate.toString());
+            if (!postCaptionTextArea.getText().isEmpty()){
+                if (p.canComment){
+                    Hyperlink commentHyperlink = new Hyperlink("View all " + p.comments.size() + " comments");
+                    post.getChildren().addAll(postOwnerHBox, postImageView, postButtonsHBox, likesHyperlink, postOwnerUsernameHyperLink, postCaptionTextArea, commentHyperlink, postDateLabel);
+                }
+                else{
+                    post.getChildren().addAll(postOwnerHBox, postImageView, postButtonsHBox, likesHyperlink, postOwnerUsernameHyperLink, postCaptionTextArea, postDateLabel);
+                }
+            }
+            else{
+                if (p.canComment){
+                    Hyperlink commentHyperlink = new Hyperlink("View all " + p.comments.size() + " comments");
+                    post.getChildren().addAll(postOwnerHBox, postImageView, postButtonsHBox, likesHyperlink, postOwnerUsernameHyperLink, commentHyperlink, postDateLabel);
+                }
+                else{
+                    post.getChildren().addAll(postOwnerHBox, postImageView, postButtonsHBox, likesHyperlink, postOwnerUsernameHyperLink, postDateLabel);
+                }
+            }
+            posts.getItems().addAll(post);
+        }
     }
 }
