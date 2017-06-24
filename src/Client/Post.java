c94d2b6ba7859425f2ffc3f97bdff797b9da42d7
Mohamad <mova.moha@yahoo.com>
2017-06-24 17:31:11 +0430

    can comment

--- src/Client/Post.java
+++ src/Client/Post.java
@@ -1,18 +1,38 @@
 package Client;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * Created by parsahejabi on 6/21/17.
  */
-public class Post {
+public class Post implements Comparable,Serializable {
     Profile owner;
     Date uploadDate;
     File image;
     ArrayList<Profile> liked;
     ArrayList<Comment> comments;
-    Comment caption;
+    String caption;
+    boolean canComment;
 
+    @Override
+    public int compareTo(Object o) {
+        return uploadDate.compareTo(((Post) o).uploadDate);
+    }
+
+    Post(Profile owner, File image, String  caption, boolean canComment)
+    {
+        this.uploadDate = new Date(System.currentTimeMillis());
+        this.owner = owner;
+        liked = new ArrayList<>();
+        this.canComment = canComment;
+        if(canComment)
+            comments = new ArrayList<>();
+        else
+            comments = new ArrayList<>(0);
+        this.image = image;
+        this.caption = caption;
+    }
 }
